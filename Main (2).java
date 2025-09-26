e27bd47f6e163e56128e5c41fd3d43c00c1dbf66
Gevorg Vart <109519914+ibmhcr34@users.noreply.github.com>
2025-09-26 11:03:02 +0300

    Add files via upload
    #ЛАБ. РАБОТА 1 РЕШЕШИЕ НА JAVA 2 ЗАДАЧА

---
+++ Main (2).java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] arr = {'a', '1', 'b', '2', 'c', '3'};
+        ArrayList<Character> letters = new ArrayList<Character>();
+        ArrayList<Character> digits = new ArrayList<Character>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z')) {
+                letters.add(arr[i]);
+            } else if (arr[i] >= '0' && arr[i] <= '9') {
+                digits.add(arr[i]);
+            }
+        }
+
+        System.out.println(letters);
+        System.out.println(digits);
+    }
+}
