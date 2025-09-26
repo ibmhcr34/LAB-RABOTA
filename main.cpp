b2fa3d8078c49092c18dc7f0e1313d078c7d3673
Gevorg Vart <109519914+ibmhcr34@users.noreply.github.com>
2025-09-26 11:06:23 +0300

    Add files via upload
    #ЛАБ. РАБОТА 1 РЕШЕШИЕ НА C++ 1 ЗАДАЧА

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "AaBbCcDd";
+    std::string upper = "";
+    std::string lower = "";
+
+    for (int i = 0; i < (int)str.size(); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            upper += str[i];
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+            lower += str[i];
+        }
+    }
+
+    std::cout << upper << std::endl;
+    std::cout << lower << std::endl;
+
+    return 0;
+}
